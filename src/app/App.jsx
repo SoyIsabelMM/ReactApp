c96587e7bdd6149a9f28fb2861aa7a5f3e8a6185
SoyIsabelMM <isabelmartinez19.30@gmail.com>
2025-12-14 23:25:22 -0300

    refactor

---
+++ src/app/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div>
+      <main className="min-h-screen bg-white "></main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header className="border-b border-border">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
+        <div>
+          <h1 className="text-2xl font-bold ">Cambiador de tema</h1>
+        </div>
+      </div>
+    </header>
+  );
+}
