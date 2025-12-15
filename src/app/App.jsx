86ff7d70017b3ee6dcbbdeb0b94c94eb740d357f
SoyIsabelMM <isabelmartinez19.30@gmail.com>
2025-12-15 00:01:40 -0300

    feat: add header component

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
+import '../components/header/Header.jsx';
+import Header from '../components/header/Header.jsx';
 
 function App() {
   return (
     <div>
-      <main className="min-h-screen bg-white "></main>
+      <main className="min-h-screen bg-white ">
+        <Header></Header>
+      </main>
     </div>
   );
 }
