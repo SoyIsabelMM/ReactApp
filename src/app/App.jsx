3ec124f5d08bb111013889692a029570116da2c3
SoyIsabelMM <isabelmartinez19.30@gmail.com>
2025-12-15 00:40:07 -0300

    feat: create logic to change the written content of the button

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react';
 import '../components/header/Header.jsx';
+import { useState } from 'react';
 import Header from '../components/header/Header.jsx';
+import ThemeToggle from '../components/themeToggle.jsx/ThemeToggle.jsx';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
   return (
     <div>
-      <main className="min-h-screen bg-white ">
-        <Header></Header>
+      <main className="min-h-screen bg-white text-zinc-900">
+        <Header>
+          <ThemeToggle theme={theme} onToggle={toggleTheme} />
+        </Header>
       </main>
     </div>
   );
