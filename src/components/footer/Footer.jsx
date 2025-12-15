cfecb67a727b9f81f9226c2d5afd1a53c8eda14b
SoyIsabelMM <isabelmartinez19.30@gmail.com>
2025-12-15 02:26:09 -0300

    feat: Create component Footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="border-t  border-zinc-300 mt-12">
+      <div className="mx-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8 text-center">
+        <p className=" text-gray-500">
+          {' '}
+          Proyecto educativo de React ● Hecho con vite + React & Tailwind
+        </p>
+      </div>
+    </footer>
+  );
+}
